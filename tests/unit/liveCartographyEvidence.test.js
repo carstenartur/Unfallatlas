1e130ef4dd2fbae5cc697534da1d0bff20541aca
Carsten Hammer <carsten.hammer@t-online.de>
2026-07-21 08:52:25 +0200

    test: reject HTTP and unexpected provider paths

--- tests/unit/liveCartographyEvidence.test.js
+++ tests/unit/liveCartographyEvidence.test.js
@@ -10,11 +10,23 @@ describe('live cartography evidence validator', () => {
   test.each([
     ['https://a.tile.openstreetmap.org/15/17030/10954.png', 'standard'],
     ['https://b.basemaps.cartocdn.com/light_only_labels/15/17030/10954.png', 'labels'],
+    ['https://b.basemaps.cartocdn.com/light_only_labels/15/17030/10954@2x.png', 'labels'],
     ['https://www.bonn.de/stadtplan-wms/services/orthofoto/MapServer/WMSServer?REQUEST=GetMap', 'orthophoto'],
     ['https://www.wms.nrw.de/geobasis/wms_nw_dop?REQUEST=GetMap', 'orthophoto'],
     ['https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/15/10954/17030', 'orthophoto']
   ])('classifies an allowed provider URL', (url, kind) => {
     expect(classifyProviderUrl(url)).toBe(kind);
+  });
+
+  test.each([
+    'http://a.tile.openstreetmap.org/15/17030/10954.png',
+    'https://a.tile.openstreetmap.org/',
+    'https://a.tile.openstreetmap.org/unexpected',
+    'https://a.tile.openstreetmap.org/15/17030/10954.svg',
+    'https://a.basemaps.cartocdn.com/light_only_labels/unexpected.png',
+    'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/not/a/tile'
+  ])('rejects non-HTTPS or unexpected provider paths', url => {
+    expect(classifyProviderUrl(url)).toBeNull();
   });
 
   test('accepts complete live raster evidence', () => {
@@ -53,6 +65,17 @@ describe('live cartography evidence validator', () => {
         }]
       },
       'not an allowed successful raster provider response'
+    ],
+    [
+      {
+        source: 'live',
+        requiredKinds: ['standard'],
+        successfulResponses: [{
+          kind: 'standard', status: 200, contentType: 'image/png',
+          url: 'http://a.tile.openstreetmap.org/15/17030/10954.png'
+        }]
+      },
+      'no successful real standard response is recorded'
     ],
     [
       {
