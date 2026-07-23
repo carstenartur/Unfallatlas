472ace698fc184baef3c6cea8499afac729a8e32
Carsten Hammer <carsten.hammer@t-online.de>
2026-07-23 08:27:31 +0200

    feat: load static map provenance export runtime

--- js/ua.core.js
+++ js/ua.core.js
@@ -154,6 +154,7 @@
         moduleUrl("ua.kml_export_provenance.js?v=2026-07-22"),
         moduleUrl("ua.document_export_provenance.js?v=2026-07-22"),
         moduleUrl("ua.document_export_prewarm.js?v=2026-07-22"),
+        moduleUrl("ua.static_map_export_provenance.js?v=2026-07-23"),
       ]).catch((error) => {
         UA.exportProvenanceError = error;
         console.error("Export-Provenienz konnte nicht initialisiert werden", error);
